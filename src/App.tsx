251f3eee6478895533200623d9e347e3295dfbf7
Bahaar Rokhva <rokhvab@gmail.com>
2019-11-26 11:27:22 -0700

    attemteped to axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Spotify from '../src/components/spotify'
+import { any } from 'prop-types';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div>
+        <Spotify />
         <a href="/login/facebook">Login with Facebook</a>
 
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
