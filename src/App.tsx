290aef9054eeedc59ba82f42fb1eb89707959805
Bahaar Rokhva <rokhvab@gmail.com>
2019-11-26 10:32:59 -0700

    added tailwind styles & npm package

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-     <div>
-       <a href="/login/facebook">Login with Facebook</a>
-       <button>Login with Spotify</button>
-       <button>Get bands</button>
-     </div>
+      <div>
+        <a href="/login/facebook">Login with Facebook</a>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Login with Spotify
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Get Bands
+        </button>
+      </div>
     </div>
   );
 }
