0595d0cb40a42e3f9a4b0658f7fb83f91aa06736
Bahaar Rokhva <rokhvab@gmail.com>
2019-11-26 20:43:59 -0700

    added splash page component file

---
+++ src/components/SplashPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Splash = () =>{
+    return(
+        <div className= "wrapper">
+            <h1>this test worked</h1>
+        </div>
+    )
+}
+
+
+
+
+
+export default Splash;
