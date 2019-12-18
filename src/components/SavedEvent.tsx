45cdc971b3e5fb573a74ad7f48990ec20a177a42
Bahaar Rokhva <rokhvab@gmail.com>
2019-12-18 10:05:55 -0700

    added styling to saved events

--- src/components/SavedEvent.tsx
+++ src/components/SavedEvent.tsx
@@ -7,19 +7,15 @@ import { A } from "hookrouter";
 const SavedEvent = ({ event }) => {
   return (
     <div className="w-1/8  m-6">
-      <div className="border-4 border-gray-400 bg-white p-4 leading-normal h-full max-w-md">
-      {/* <img
-          alt="artist"
-          className="artist-img border-black border-8 w-1/2 mt-2"
-          style={{backgroundImage: "url(https://assets.sk-static.com/images/default_images/huge_avatar/default-event.png)", backgroundSize: "cover"}}
-          src={`https://images.sk-static.com/images/media/profile_images/artists/${event.id}/huge_avatar`} /> */}
+      <div className="border-4 border-black  p-4 leading-normal h-full max-w-md">
+      
         <div className="">
         
 
-          <div className="text-gray-900 font-bold text-xl mb-4 h-full">
+          <div className="text-black font-bold text-xl pb-6 m-6 mb-4 h-full border-b-4 border-teal-300">
             <A href={`/event-details/${event.id}`}>{event.displayName}</A>
           </div>
-          <div className="text-gray-700 text-base">
+          <div className=" m-6 text-black text-base leading-loose">
             <p>Location: {event.location.city}</p>
             <p>Venue: {event.venue.displayName}</p>
             <p>Starts: {moment(event.start.datetime || event.start.date).format("ddd MMM DD, YYYY")}</p>
